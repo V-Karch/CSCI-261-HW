cf92b284eb0a7d66bd3fb83f82f061ecd005ca1e
V-Karch <luna.karch05@gmail.com>
2026-01-14 12:47:44 -0500

    Create initial program to read the amount of loops needed

--- hw00/smallesttwo.cpp
+++ hw00/smallesttwo.cpp
@@ -1,1 +1,11 @@
 // Solve this for homework
+#include <iostream>
+
+int main(void) {
+    int amount_of_input;
+    scanf("%d", &amount_of_input);
+
+    std::cout << "I should loop " << amount_of_input << " times\n";
+    return 0;
+}
+
